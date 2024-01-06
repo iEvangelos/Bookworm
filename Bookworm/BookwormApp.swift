eb4ac3817955246b6bd219b7792dcf4947138a45
Evan Pipis <ievangelos@icloud.com>
2024-01-06 20:24:18 +0000

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Evangelos Pipis on 06/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
